16e560807b22883368967197ed1fab3ea687bc4e
Crowdbotics <team@crowdbotics.com>
2023-12-16 19:47:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "boundarybuddy-45055",
+  url: "https://boundarybuddy-45055.botics.co",
+  api: "https://boundarybuddy-45055.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
